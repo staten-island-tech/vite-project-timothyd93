af1448015ead2b3e8e808dc86f2febe5eb9fe22f
timothyd93 <113132240+timothyd93@users.noreply.github.com>
2022-12-06 13:59:11 -0500

    array

--- vite-project/js/array.js
+++ vite-project/js/array.js
@@ -1,1 +1,44 @@
-// export { array };
+// export { books };
+const books = [
+  {
+    name: "The Book Thief",
+    price: 6.99,
+    genre: [
+      "Novel",
+      "Young Adult Fiction",
+      "Historical Fiction",
+      "Bildungsroman",
+    ],
+    avgrating: 4.6,
+    author: "Markus Zusak",
+  },
+  {
+    name: "To Kill a Mockingbird",
+    price: 10.99,
+    genre: [
+      "Novel",
+      "Bildungsroman",
+      "Thriller",
+      "Southern Gothic",
+      "Domestic Fiction",
+      "Legal Story",
+    ],
+    avgrating: 4.27,
+    author: "Harper Lee",
+  },
+  {
+    name: "Animal Farm",
+    price: 15.1,
+    genre: [
+      "Allegory",
+      "Satire",
+      "Fable",
+      "Novella",
+      "Roman à clef",
+      "Dystopian Fiction",
+      "Political Fiction",
+    ],
+    avgrating: 3.98,
+    author: "George Orwell",
+  },
+];
