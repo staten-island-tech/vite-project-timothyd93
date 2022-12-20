83e00e5617e499387e148fbd0b2c2e0f4a38b9a7
timothyd93 <113132240+timothyd93@users.noreply.github.com>
2022-12-20 13:51:56 -0500

    b

--- vite-project/js/array.js
+++ vite-project/js/array.js
@@ -1,4 +1,3 @@
-import photo from "../public";
 const array = [
   {
     name: "The Book Thief",
@@ -12,7 +11,7 @@ const array = [
     avgrating: 4.61,
     author: "Markus Zusak",
     published: 2005,
-    img: photo.bookthief,
+    img: `https://static-readingaustralia-com-au.s3.amazonaws.com/uploads/2018/01/The-Book-Thief-cover-image.jpg`,
   },
   {
     name: "To Kill a Mockingbird",
@@ -28,7 +27,7 @@ const array = [
     avgrating: 4.27,
     author: "Harper Lee",
     published: 1960,
-    img: photo.mockingbird,
+    img: `https://media.wired.com/photos/593238fcedfced5820d0f04f/master/w_1600%2Cc_limit/to-kill-a-mockingbird.jpg`,
   },
   {
     name: "Animal Farm",
@@ -45,7 +44,7 @@ const array = [
     avgrating: 4.01,
     author: "George Orwell",
     published: 1945,
-    img: photo.animalfarm,
+    img: `https://i.pinimg.com/originals/a9/33/fa/a933fa4e5302778a02b48918bacb81db.jpg`,
   },
   {
     name: "Invisible Man",
@@ -54,7 +53,7 @@ const array = [
     avgrating: 4.48,
     author: "Ralph Ellison",
     published: 1952,
-    img: photo.invisibleman,
+    img: `https://s26162.pcdn.co/wp-content/uploads/2019/04/14395257572.jpg`,
   },
   {
     name: "To The Lighthouse",
@@ -63,7 +62,7 @@ const array = [
     avgrating: 3.93,
     author: "Virginia Woolf",
     published: 1927,
-    img: photo.tothelighthouse,
+    img: `https://m.media-amazon.com/images/I/41GWqsNHAPL.jpg`,
   },
   {
     name: "Lord of The Flies",
@@ -72,7 +71,7 @@ const array = [
     avgrating: 3.52,
     author: "William Golding",
     published: 1954,
-    img: photo.lordoftheflies,
+    img: `https://m.media-amazon.com/images/I/51eeAWItwbL._AC_SY780_.jpg`,
   },
 ];
 export { array };
--- vite-project/js/index.js
+++ vite-project/js/index.js
@@ -2,25 +2,8 @@ import "../styles/style.css";
 import { DOMSelectors } from "./dom";
 import { array } from "./array";
 
-function roman() {
-  document.getElementById("romanbtn").style.backgroundColor = "#d4af37";
-  array
-    .filter((array) => array.genre.includes("Bildungsroman"))
-    .forEach((book) => {
-      let name = book.name;
-      let price = book.price;
-      let avgrating = book.avgrating;
-      let author = book.author;
-      let published = book.published;
-      let img = book.img;
-      DOMSelectors.content.insertAdjacentHTML(
-        "beforeend",
-        `<div class=books> <img src= ${img} class="cover"> <p>Book: ${name}</p> <p>Price: $${price}</p> <p>Rating: ${avgrating} </p><p>Author: ${author}</p> <p>Date Published: ${published}</p></div>`
-      );
-    });
-}
-function injectCards() {
-  document.getElementById("bookbtn").style.backgroundColor = "#d4af37";
+function injectBooks() {
+  DOMSelectors.bookbtn.style.backgroundColor = "#d4af37";
   array.forEach((book) => {
     let name = book.name;
     let price = book.price;
@@ -36,7 +19,7 @@ function injectCards() {
 }
 
 function bestrated() {
-  document.getElementById("bestratedbtn").style.backgroundColor = "#d4af37";
+  DOMSelectors.bestratedbtn.style.backgroundColor = "#d4af37";
   array
     .filter((array) => array.avgrating >= 4.0)
     .forEach((book) => {
@@ -52,35 +35,54 @@ function bestrated() {
       );
     });
 }
-function removeCards() {
-  const book = document.querySelectorAll(".books");
-  const bookarray = Array.from(book);
-  bookarray.forEach((book) => book.remove());
-  document.getElementById("bestratedbtn").style.backgroundColor = "";
-  document.getElementById("bookbtn").style.backgroundColor = "";
-  document.getElementById("romanbtn").style.backgroundColor = "";
+
+function roman() {
+  DOMSelectors.romanbtn.style.backgroundColor = "#d4af37";
+  array
+    .filter((array) => array.genre.includes("Bildungsroman"))
+    .forEach((book) => {
+      let name = book.name;
+      let price = book.price;
+      let avgrating = book.avgrating;
+      let author = book.author;
+      let published = book.published;
+      let img = book.img;
+      DOMSelectors.content.insertAdjacentHTML(
+        "beforeend",
+        `<div class=books> <img src= ${img} class="cover"> <p>Book: ${name}</p> <p>Price: $${price}</p> <p>Rating: ${avgrating} </p><p>Author: ${author}</p> <p>Date Published: ${published}</p></div>`
+      );
+    });
 }
 
-injectCards();
+function removeBooks() {
+  const book = document.querySelectorAll(".books");
+  const bookArray = Array.from(book);
+  bookArray.forEach((book) => book.remove());
+  DOMSelectors.bestratedbtn.style.backgroundColor = "";
+  DOMSelectors.bookbtn.style.backgroundColor = "";
+  DOMSelectors.romanbtn.style.backgroundColor = "";
+}
+
+injectBooks();
 
 DOMSelectors.colorbtn.addEventListener("click", function () {
-  if (document.body.classList.contains("sky")) {
+  if (document.body.classList.contains("winter")) {
     document.body.classList.add("forest");
-    document.body.classList.remove("sky");
+    document.body.classList.remove("winter");
   } else {
-    document.body.classList.add("sky");
+    document.body.classList.add("winter");
     document.body.classList.remove("forest");
   }
 });
 DOMSelectors.bestratedbtn.addEventListener("click", function () {
-  removeCards();
+  removeBooks();
   bestrated();
 });
 DOMSelectors.bookbtn.addEventListener("click", function () {
-  removeCards();
-  injectCards();
+  removeBooks();
+  injectBooks();
 });
 DOMSelectors.romanbtn.addEventListener("click", function () {
-  removeCards();
+  removeBooks();
   roman();
 });
